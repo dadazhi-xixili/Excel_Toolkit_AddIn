cdc1ab572aae2471441a17e26191f1c0af41c30c
大大只的细细粒 <again9527@outlook.com>
2025-07-31 11:22:34 +0800

    构建Xml类 方便后续动态调整功能区结构

--- Ribbon.cs
+++ Ribbon.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -22,30 +23,14 @@ namespace Excel_Toolkit
 
         public string GetCustomUI(string ribbonID)
         {
-            System.Diagnostics.Debug.WriteLine($"GetCustomUI called with ribbonID: {ribbonID}");
             layout = Globals.ThisAddIn.layout;
             layout.ribbon = this;
             level1 = layout.level1;
-            string buttons = "";
-            for (int i = 0; i < level1.Length; i++)
-            {
-                string label = level1[i];
-                buttons += $"<button id=\"{label}\" label=\"{label}\" onAction=\"Level1ButtonClick\"/>\n";
-            }
-            string xml =
-                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-                 "<customUI xmlns=\"http://schemas.microsoft.com/office/2009/07/customui\" onLoad=\"Ribbon_Load\">" +
-                 "<ribbon>" +
-                 "<tabs>" +
-                 "<tab id=\"Toolkit\" label=\"Toolkit\">" +
-                 "<group id=\"Group1\" label=\"功能组\">" +
-                 buttons +
-                 "</group>" +
-                 "</tab>" +
-                 "</tabs>" +
-                 "</ribbon>" +
-                 "</customUI>";
-            return xml;
+            Xml.IControl[]  buttons = level1.Select(label => (Xml.IControl)new Xml.Button(label, label, "Level1ButtonClick")).ToArray();
+            Xml.Group[] groups = { new Xml.Group("函数分组", "函数分组", buttons) };
+            Xml.Tab[] tabs = { new Xml.Tab("Toolkit", "Toolkit", groups) };
+            Xml xml = new Xml(tabs);
+            return xml.ToXml();
         }
         #endregion
 
@@ -59,5 +44,103 @@ namespace Excel_Toolkit
             layout.Level1ButtonClick(control.Id);
         }
         #endregion
+
+        public class Xml
+        {
+            public string header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><customUI xmlns=\"http://schemas.microsoft.com/office/2009/07/customui\" onLoad=\"Ribbon_Load\"><ribbon><tabs>";
+            public string footer = "</tabs></ribbon></customUI>";
+            public Tab[] tabs;
+            public Xml(Tab[] tabs)
+            {
+                this.tabs = tabs;
+            }
+            public string ToXml()
+            {
+                string xml = header;
+                for (int i = 0; i < this.tabs.Length; i++)
+                {
+                    xml += this.tabs[i].ToXml();
+                }
+                xml += footer;
+                return xml;
+            }
+            public struct Tab
+            {
+                string id;
+                string label;
+                Group[] groups;
+                public Tab(string id, string label, Group[] groups)
+                {
+                    this.id = id;
+                    this.label = label;
+                    this.groups = groups;
+                }
+                public string ToXml()
+                {
+                    string xml = $@"<tab id=""{id}"" label=""{label}"">";
+                    foreach (Group group in this.groups)
+                    {
+                        xml += group.ToXml();
+                    }
+                    xml += "</tab>";
+                    return xml;
+                }
+            }
+            public interface IControl
+            {
+                string ToXml();
+            }
+            public struct Group
+            {
+                string id;
+                string label;
+                IControl[] controls;
+                public Group(string id, string label, IControl[] controls)
+                {
+                    this.id = id;
+                    this.label = label;
+                    this.controls = controls;
+                }
+                public string ToXml()
+                {
+                    string xml = $@"<group id=""{id}"" label=""{label}"">";
+                    foreach (IControl control in this.controls)
+                    {
+                        xml += control.ToXml();
+                    }
+                    xml+= "</group>";
+                    return xml;
+                }
+
+            }
+            public struct Button : IControl
+            {
+                string id;
+                string label;
+                string onAction;
+                public Button(string id, string label, string onAction)
+                {
+                    this.id = id;
+                    this.label = label;
+                    this.onAction = onAction;
+                }
+                public string ToXml()
+                {
+                    return $"<button id=\"{id}\" label=\"{label}\" onAction=\"{onAction}\" />";
+                }
+            }
+             public struct Separator : IControl
+    {
+        string id;
+        public Separator(string id)
+        {
+            this.id = id;
+        }
+        public string ToXml()
+        {
+            return $"<separator id=\"{id}\" />";
+        }
+    }
+        }
     }
 }
